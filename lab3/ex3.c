1ffaf955ee0109093de7acb8be680c8271dc3367
Stas Ivashkov <100317434+drkrops@users.noreply.github.com>
2024-03-09 10:49:08 +0000

    ex3

---
+++ lab3/ex3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+unsigned int round_down(unsigned int x, unsigned int D) {
+    return x & ~(D - 1);
+}
+
+unsigned int round_up(unsigned int x, unsigned int D) {
+    return (x + (D - 1)) & ~(D - 1);
+}
+
+int main() {
+    unsigned int x = 5;
+    unsigned int D = 128;
+
+    printf("Исходное значение: %u\n", x);
+    printf("Округление вниз: %u\n", round_down(x, D));
+    printf("Округление вверх: %u\n", round_up(x, D));
+
+    return 0;
+}
