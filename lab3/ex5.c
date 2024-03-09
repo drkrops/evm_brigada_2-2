4c920d3764f1fb745a2cdf63b99da842f4c5c51c
Stas Ivashkov <100317434+drkrops@users.noreply.github.com>
2024-03-09 11:00:46 +0000

    ex5

---
+++ lab3/ex5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float abs_float(float x) {
+    int *ptr = (int *)&x;
+    *ptr &= 0x7FFFFFFF;
+    float result = *((float *)ptr);
+    return result;
+}
+
+int main() {
+    float x = -13.2;
+    printf("Модуль числа %.2f равен %.2f\n", x, abs_float(x));
+
+    return 0;
+}
